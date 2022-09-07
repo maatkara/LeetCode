24c630a821c448c94b30e4012aaafda303283c6a
maatkara <elena-vo@yandex.ru>
2022-09-07 15:12:30 +0400

    rewrite

--- utils/binary_tree.py
+++ utils/binary_tree.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -5,35 +8,53 @@ class TreeNode:
         self.right = right
 
     def __str__(self):
-        return f'Node val: {self.val}, left: {self.left}, right: {self.right}'
+        return f'val: {self.val}, left: {self.left}, right: {self.right}'
 
 
 class BinaryTree:
     def __init__(self):
         self.root = None
+        self.size = 0
 
     def make_tree(self, root_l: list):
-        n = len(root_l)
-        children = [None] * n  # left child index
 
-        for i, el in enumerate(root_l):
-            #  for not reversed
-            if i < n // 2:
-                children[i] = 2 * i + 1
+        root_l = [self.add_node(el) for el in root_l]
+        self.size = len(root_l)
+        i = 0
+        j = 1
 
-        for i, el in enumerate(reversed(root_l)):
+        while i < self.size:
 
-            if el is None:
-                continue
+            node = root_l[i]
 
-            node = TreeNode(el)
-
-            j = n - 1 - i
-
-            if children[j] and el:
-                node.left = root_l[children[j]]
-                node.right = root_l[children[j] + 1]
-
-            root_l[n - i - 1] = node
+            if node and j < self.size:
+                node.left = root_l[j]
+                j += 1
+                if j < self.size:
+                    node.right = root_l[j]
+                j += 1
+            i += 1
 
         self.root = root_l[0]
+
+    @staticmethod
+    def add_node(x):
+        return TreeNode(x) if x is not None else None
+
+
+def tree_by_level(root: TreeNode) -> defaultdict:
+    root_levels = defaultdict(list)
+    root_levels[0] = [root.val]
+
+    def dfs(node: TreeNode, i=0):
+        nonlocal root_levels
+
+        if not node:
+            return
+
+        for ch in (node.left, node.right):
+            root_levels[i + 1] += [ch.val] if ch else [None]
+            dfs(ch, i + 1)
+
+    dfs(root)
+    return root_levels
