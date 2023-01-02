fb4c962e2e4df737d810cf018f3e0f43b54dc443
maatkara <elena-vo@yandex.ru>
2023-01-02 18:01:39 +0400

    change testing function

--- easy/middle_of_the_linked_list_876.py
+++ easy/middle_of_the_linked_list_876.py
@@ -8,56 +8,48 @@ from utils.linked_list import bild_linked_list, ListNode
 """
 876. Middle of the Linked List
 https://leetcode.com/problems/middle-of-the-linked-list/
+Easy
 
 Given the head of a singly linked list, return the middle node of the linked list.
 If there are two middle nodes, return the second middle node.
 
-Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
- 
-
 Example 1:
-
+--------------
 Input: head = [1,2,3,4,5]
 Output: [3,4,5]
 Explanation: The middle node of the list is node 3.
 
 Example 2:
-
+--------------
 Input: head = [1,2,3,4,5,6]
 Output: [4,5,6]
 Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
  
 
 Constraints:
+--------------
 The number of nodes in the list is in the range [1, 100].
 1 <= Node.val <= 100
+
+18.8.2021
 """
 
 N_MAX = int(1e2)
 X_MAX = 100
 
 
-def middle_node(arr: list, is_debug=False) -> ListNode:
+def middle_node(arr: list) -> ListNode:
     """ Return the middle node of the singly linked list """
-    head = bild_linked_list(arr)
+    head = bild_linked_list(arr)  # for testing only | not for LC
+    # ---------------------------
 
     fast = head
     slow = head
 
-    if is_debug:
-        print(f'\n{arr}')
     while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
-        if is_debug:
-            print(f'fast: {fast.val}, slow: {slow.val}' if fast else f'fast: {fast}, slow: {slow.val}')
 
-    if is_debug:
-        print(f'Linked list: {head}')
     return slow
 
 
