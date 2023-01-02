e634b7d093e0b622f98af0c0d9aaf3fdaaf2748e
maatkara <elena-vo@yandex.ru>
2023-01-02 17:33:32 +0400

    rename prev to cur

--- easy/merge-two-sorted-lists-21.py
+++ easy/merge-two-sorted-lists-21.py
@@ -52,23 +52,23 @@ def merge_two_lists(llist1: list, llist2: list) -> Optional[ListNode]:
     # ----------------------
 
     dummy = ListNode(None)
-    prev = dummy
+    cur = dummy
 
     while list1 and list2:
         if list1.val > list2.val:
-            prev.next = list2
+            cur.next = list2
             list2 = list2.next
         else:
-            prev.next = list1
+            cur.next = list1
             list1 = list1.next
 
-        prev = prev.next
+        cur = cur.next
 
     if list1:
-        prev.next = list1
+        cur.next = list1
 
     if list2:
-        prev.next = list2
+        cur.next = list2
 
     return dummy.next
 
