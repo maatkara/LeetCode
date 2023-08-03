d28240f21bcaa1c1a6b93b68bc5cea18f0d1e364
maatkara <elena-vo@yandex.ru>
2023-08-03 17:19:34 +0400

    cosmetic

--- medium/integer-to-roman-12.py
+++ medium/integer-to-roman-12.py
@@ -138,7 +138,7 @@ def test_readme():
     from utils.make_string import get_readme
 
     topic = 'Array'
-    file_name = 'roman_to_integer_13.py'
+    file_name = '../easy/roman_to_integer_13.py'
 
     print('\n')
     print(get_readme(string_, topic, file_name))
